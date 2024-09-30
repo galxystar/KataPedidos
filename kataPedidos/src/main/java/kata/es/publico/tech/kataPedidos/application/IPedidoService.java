4f17258bed77bfe5e1791f6f0a06f9946fa48008
galxyStar <emilianvornicu@gmail.com>
2024-09-30 15:40:37 +0200

    añado interfaz al commit anterior

--- kataPedidos/src/main/java/kata/es/publico/tech/kataPedidos/application/IPedidoService.java
+++ kataPedidos/src/main/java/kata/es/publico/tech/kataPedidos/application/IPedidoService.java
@@ -4,7 +4,7 @@ import java.util.List;
 import kata.es.publico.tech.kataPedidos.domain.Pedido;
 
 /**
- * Interface para la obtención de pedidos desde el servicio web.
+ * Interfaz para la obtención de pedidos desde el servicio web.
  * Proporciona un método para obtener todos los pedidos disponibles.
  */
 public interface IPedidoService {
@@ -13,8 +13,10 @@ public interface IPedidoService {
      * Obtiene todos los pedidos disponibles en el servicio web.
      * Este método incluye la lógica de paginación para obtener grandes cantidades de datos.
      *
+     * @param pagina Número de la página a obtener. Si es {@code null}, comenzará desde la página 1.
+     * @param maxPorPagina Máximo número de pedidos a recuperar por página. Si es {@code null}, se establece a 100.
      * @return Una lista de objetos {@link Pedido} obtenidos desde el servicio web.
      * @throws Exception Si ocurre un error durante la solicitud o el procesamiento de los pedidos.
      */
-    public List<Pedido> getAllPedidos() throws Exception;
+    public List<Pedido> getAllPedidos(Integer pagina, Integer maxPorPagina) throws Exception;
 }
